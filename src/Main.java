ac836045454a2c2bb132b424e792f2d3fbac8f12
MuyiwaAbim <oabimbola123@gmail.com>
2022-08-04 00:51:28 -0400

    Updated

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main
 {
     public static void main(String[] args) {
-
+        // loop to control the rows
         for(int i = 5; i >= 1; i--)
         {
-
+            // loop to control the columns
             for(int j = 1; j <= i; j++)
             {
                 System.out.print("*");
